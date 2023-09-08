08d9b0edf63bacad2fb3881dac9409ede6de2c61
Gonzalo Cozzo <gonza007_88@hotmail.com>
2023-09-08 19:59:57 -0300

    creada la clase archivo para la creacion del mismo

--- src/ventanas/Archivo.java
+++ src/ventanas/Archivo.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ventanas;
 
-/**
- *
- * @author GONZALO
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class Archivo {
     
+    private File archivo;
+    
+    
+    public void crearArchivo() {
+        archivo = new File("agendaContactos.txt");
+        
+        try {
+            if(archivo.createNewFile()) {
+                System.out.println("Archivo creado");
+            }
+        } catch (IOException ex) {
+            System.err.println("Error, "+ ex);
+        }
+    }
 }
